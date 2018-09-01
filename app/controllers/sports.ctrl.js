a86a61002e21e1ee693a6aadbae205f4b021ae3d
Adriana Bonilla <apbonillab@gmail.com>
2018-08-31 20:23:09 -0500

    add services

--- app/controllers/sports.ctrl.js
+++ app/controllers/sports.ctrl.js
@@ -3,11 +3,17 @@ var express = require('express')
 var router = express.Router();
 var sportService = require('../services/sport.srv.js');
 
-router.get('/sports',function(req,rs){
+router.get('/sports',function(req,res){
     sportService.getAllSport(function(sports){
         res.statusCode = 200;
         res.send(sports);
     })
 });
 
+router.get('/athlets',function(req,res){
+    sportService.getAllAthlets(function(athlets){
+        res.statusCode = 200;
+        res.send(athlets);
+    })
+})
 module.exports = router;
--- app/services/sport.srv.js
+++ app/services/sport.srv.js
@@ -1,7 +1,12 @@
 'use strict'
 
 const sportList = require('../../assets/sports.json');
+const athletsList = require('../../assets/Athlets.json');
 
 module.exports.getAllSport = function(success,error){
     success(sportList);
+}
+
+module.exports.getAllAthlets = function(success,error){
+    success(athletsList);
 }
